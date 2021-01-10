b087b8365bd252dba0fc881db15ab364cd3b009f
schijoh <johannaschiele@hks.harvard.edu>
2021-01-10 16:00:52 +0100

    added content to main page, made lit review page, changed some styles

--- main.js
+++ main.js
@@ -8,3 +8,20 @@ var logo = document.querySelector('img');
 
 // Assign the function to the onclick event on that element
 logo.onclick = sayOuch;
+
+// Include literature review table
+d3.text("lit_review_table.csv", function(data) {
+    var parsedCSV = d3.csv.parseRows(data);
+
+    var container = d3.select("#lit_review")
+        .append("table")
+
+        .selectAll("tr")
+            .data(parsedCSV).enter()
+            .append("tr")
+
+        .selectAll("td")
+            .data(function(d) { return d; }).enter()
+            .append("td")
+            .text(function(d) { return d; });
+});
