822cbeeda1eb714cc37f9b6d7c88acd30a052cae
schijoh <johannaschiele@hks.harvard.edu>
2021-01-14 15:38:36 +0100

    made first working collapsible / dropdown text

--- main.js
+++ main.js
@@ -1,22 +1,4 @@
 
-// Include literature review table
-// d3.text("lit_review_table.csv", function(data) {
-//     var parsedCSV = d3.csv.parseRows(data);
-
-//     var container = d3.select("#lit_review")
-//         .append("table")
-
-//         .selectAll("tr")
-//             .data(parsedCSV).enter()
-//             .append("tr")
-
-//         .selectAll("td")
-//             .data(function(d) { return d; }).enter()
-//             .append("td")
-//             .text(function(d) { return d; });
-// });
-
-
 // collapsible content our team page
 
 var acc = document.getElementsByClassName("accordion");
