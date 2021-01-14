1cb6103a734b8a1a7c2ad7ab894c6989d0e577e7
Sam Shaffer <sshaffer@hks.harvard.edu>
2021-01-14 07:54:59 -0500

    delete broken JS code from old element

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 
 // Include literature review table
-d3.text("lit_review_table.csv", function(data) {
-    var parsedCSV = d3.csv.parseRows(data);
+// d3.text("lit_review_table.csv", function(data) {
+//     var parsedCSV = d3.csv.parseRows(data);
 
-    var container = d3.select("#lit_review")
-        .append("table")
+//     var container = d3.select("#lit_review")
+//         .append("table")
 
-        .selectAll("tr")
-            .data(parsedCSV).enter()
-            .append("tr")
+//         .selectAll("tr")
+//             .data(parsedCSV).enter()
+//             .append("tr")
 
-        .selectAll("td")
-            .data(function(d) { return d; }).enter()
-            .append("td")
-            .text(function(d) { return d; });
-});
+//         .selectAll("td")
+//             .data(function(d) { return d; }).enter()
+//             .append("td")
+//             .text(function(d) { return d; });
+// });
 
 
 // collapsible content our team page
